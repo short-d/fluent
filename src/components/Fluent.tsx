69eac93a7825e69f73c159aa24b26e4e70288dbc
Harry Liu <byliuyang11@gmail.com>
2020-05-28 23:03:25 -0700

    Add empty editor

--- src/components/Fluent.tsx
+++ src/components/Fluent.tsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
-import './Fluent.scss';
+import styles from './Fluent.module.scss';
+import { Editor } from './Editor';
 
 export class Fluent extends Component<any, any> {
     public render() {
-        return <div>Fluent 123</div>;
+        return (
+            <div className={styles.Fluent}>
+                <Editor />
+            </div>
+        );
     }
 }
