a008b5db5d783009c69da18d501f1c891dc35f79
yibolu <56810436+yibolu@users.noreply.github.com>
2020-08-23 16:46:38 -0400

    Create a beginning verison of Selection Service (#14)
    * Adding beginning version of Selection Service for Fluent
    
    * change for pr

--- src/components/Fluent.tsx
+++ src/components/Fluent.tsx
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 import styles from './Fluent.module.scss';
 import Editor from './Editor';
 import { IEditorStateService } from '../services/editor-state.service';
+import { SelectionService } from '../services/selection.service';
 
 export class Fluent extends Component<any, any> {
     private editorStateService = new IEditorStateService();
-
+    private selectionService = new SelectionService();
+    
     public render() {
         return (
             <div className={styles.Fluent}>
-                <Editor stateService={this.editorStateService} />
+                <Editor
+                    stateService={this.editorStateService}
+                    selectionService={this.selectionService}
+                />
             </div>
         );
     }
